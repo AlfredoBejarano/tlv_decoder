60e281457f0b8ff3e9e5b7a24158e16e6524ba98
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-11 22:17:44 -0500

    Create TLVRecordsAdapter.kt
    Adapter class that tells to a RecyclerView how to render TLV values

---
+++ app/src/main/java/me/alfredobejarano/tlv/adapter/TLVRecordsAdapter.kt
@@ -0,0 +1,55 @@
+package me.alfredobejarano.tlv.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
+import me.alfredobejarano.tlv.R
+import me.alfredobejarano.tlv.data.TLV
+
+/**
+ *
+ * Adapter that displays a list of [TLV] objects in a RecyclerView.
+ *
+ * @author Alfredo Bejarano
+ * @since 11/10/2018 - 09:59 PM
+ * @version 1.0
+ **/
+class TLVRecordsAdapter(private var records: List<TLV>?) :
+    RecyclerView.Adapter<TLVRecordsAdapter.TLVRecordViewHolder>() {
+
+    /**
+     * Creates a [TLVRecordViewHolder] object to be added to the RecyclerView.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        TLVRecordViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_tlv, parent, false)
+        )
+
+    /**
+     * Retrieves the amount of records that the RecyclerView is going to display.
+     */
+    override fun getItemCount() = records?.size ?: 0
+
+    /**
+     * Assign values to the ViewHolder views.
+     */
+    override fun onBindViewHolder(holder: TLVRecordViewHolder, position: Int) {
+        holder.valueView.setText(records?.get(position)?.value ?: "")
+        holder.valueView.setHint(records?.get(position)?.tag ?: R.string.unknown_tag)
+    }
+
+    fun setResults(results: List<TLV>) {
+        records = results
+        notifyDataSetChanged()
+    }
+
+    /**
+     * ViewHolder subclass that defines how a TLV record is going to be drawn.
+     */
+    class TLVRecordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val valueView: TextInputEditText = itemView.findViewById(R.id.tlv_value)
+    }
+}
