1cd7b0789b9aff5e145ecda159b4522b52a4142e
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-11 23:45:20 -0500

    Update HexUtils.kt
    Fix algorithm for parsing a Hex string into text

--- app/src/main/java/me/alfredobejarano/tlv/utilities/HexUtils.kt
+++ app/src/main/java/me/alfredobejarano/tlv/utilities/HexUtils.kt
@@ -1,7 +1,5 @@
 package me.alfredobejarano.tlv.utilities
 
-import java.nio.charset.Charset
-
 /**
  *
  * Utils class that provides methods for parsing hex values
@@ -15,4 +13,13 @@ import java.nio.charset.Charset
 /**
  * Parses a String in hexadecimal to a readable string.
  */
-fun String.fromHex() = String(this.toByteArray(), Charset.forName("UTF-8"))
+fun String.fromHex(): String {
+    val output = StringBuilder()
+    var i = 0
+    while (i < this.length) {
+        val str = this.substring(i, i + 2)
+        output.append(Integer.parseInt(str, 16).toChar())
+        i += 2
+    }
+    return output.toString()
+}
